8338298e968491d867a66acb8f765ab406fe9e2b
KrasilnikovIvan <krasilnikovivan11@mail.ru>
2018-10-21 00:01:42 +0300

    realized class Numbers

--- src/ru/krasilnikovivan11/Numbers.java
+++ src/ru/krasilnikovivan11/Numbers.java
@@ -1,5 +1,25 @@
 package ru.krasilnikovivan11;
 
-public class Numbers {
+import java.util.HashMap;
 
+public class Numbers {
+private HashMap<String,String> numbers;
+public Numbers() {
+	numbers = new HashMap<String,String>();
+}
+public void add(String number,String name ) {
+	numbers.put(number, name);
+}
+public void get(String name) {
+	StringBuilder sb = new StringBuilder();
+	if(numbers.containsValue(name)) {
+		for(String str : numbers.keySet()) {
+			if(name.equals(numbers.get(str))) {
+				sb.append(str + " ");
+			}
+		}
+		System.out.print(sb.toString());
+	}
+	else System.out.print("The member doesnt exist");
+}
 }
