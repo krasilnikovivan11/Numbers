45bce6b2a4c0a6cbfd23acc6337e104ffefa9e47
KrasilnikovIvan <krasilnikovivan11@mail.ru>
2018-10-21 00:03:37 +0300

    Main class realized phonebook

--- src/ru/krasilnikovivan11/Main.java
+++ src/ru/krasilnikovivan11/Main.java
@@ -3,7 +3,12 @@ package ru.krasilnikovivan11;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Numbers n1 = new Numbers();
+		n1.add("12345678", "Ivan");
+		n1.add("12qrt8", "Ivan");
+		n1.add("12reyttuyr678", "Oleg");
+		n1.add("12tyituyiu8", "Dima");
+		n1.get("Dimon");
 
 	}
 
